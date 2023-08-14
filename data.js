d332cbbc80d2cf8651cd2182763e13113fe0e891
Dan <dvwhite1981@gmail.com>
2023-08-14 17:54:39 -0400

    Complete

--- data.js
+++ data.js
@@ -1,1 +1,21 @@
 // Your code here
+window.addEventListener("DOMContentLoaded", event => {
+    addToList();
+});
+
+const addToList = () => {
+    const addButton = document.getElementById("add");
+    const shoppingList = document.getElementById("shopping-list");
+    const itemName = document.getElementById("name");
+    const itemType = document.getElementById("type");
+
+    addButton.addEventListener("click", event => {
+        event.preventDefault();
+
+        const listItem = document.createElement("li");
+        listItem.setAttribute("data-type", itemType.value);
+        listItem.innerText = itemName.value;
+
+        shoppingList.append(listItem);
+    });
+}
